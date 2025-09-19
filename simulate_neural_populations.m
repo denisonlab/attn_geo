ffcacaf27295636cd6de0181a3a1e7e700061d68
afchapman <angusdotchapman@gmail.com>
2025-09-19 11:01:40 -0400

    finalize population models

--- simulate_neural_populations.m
+++ simulate_neural_populations.m
@@ -1,92 +1,88 @@
-ang = 2:2:180; % orientations
-centers = 4:4:180; % preferred orientations
+%% neural population models of representational geometry
 
-amp = 1;%abs(abs(sind(2*centers))-1).^2; % cardinal biases
+% define the feature space
+ang = -89:90; % orientations
+centers = -86:4:90; % preferred orientations
 
-k = 2; % von-mises concentration parameter
-npop = exp(k.*cosd(2*(ang - centers'))) .* (amp'+2);
+k = 4; % von-mises concentration parameter
+npop = exp(k.*cosd(2*(ang - centers')));
 
 sigma = 0;
 npop = npop ./ (sigma+sum(npop,1)); % normalize population response
 
 % simulate attentional gain
-gain = 1;
-attn = 1 + gain .* cosd(ang-45).^16;
+gain = 0.6;
+n = 20; % control width of attention
+targ = -45; % target orientation
+attn = 1 + gain .* cosd(ang-targ).^n;
 
 npop_attn = npop .* attn(ismember(ang,centers))';
 
 %% simulate triad responses
-tic
-all_stim = combvec(1:length(ang),1:length(ang),1:length(ang));
-all_p0 = nan(length(ang),length(ang),length(ang));
-all_p1 = nan(length(ang),length(ang),length(ang));
-all_p2 = nan(length(ang),length(ang),length(ang));
+
+triad_stim = -89:90; % orientations to simulate triads over
+all_stim = combvec(1:length(triad_stim),1:length(triad_stim),1:length(triad_stim));
+
+% preallocate arrays
+all_p0 = zeros(length(triad_stim).*ones(1,3));
+all_p1 = zeros(length(triad_stim).*ones(1,3));
+all_p2 = zeros(length(triad_stim).*ones(1,3));
+
 for ii=1:length(all_stim)
-    this_resp = npop_attn(:,all_stim(:,ii));
-    norm_resp = 3 .* this_resp ./ sum(this_resp(:));
+    % get population response to the triad
+    [this_stim,~] = find(triad_stim(all_stim(:,ii))==ang');
+    this_resp = npop_attn(:,this_stim);
+    norm_resp = 3 .* this_resp ./ sum(this_resp,'all');
+
     all_p1(all_stim(1,ii),all_stim(2,ii),all_stim(3,ii)) = vecnorm(this_resp(:,1)-this_resp(:,2));
     all_p2(all_stim(1,ii),all_stim(2,ii),all_stim(3,ii)) = vecnorm(norm_resp(:,1)-norm_resp(:,2));
 
-    this_resp0 = npop(:,all_stim(:,ii));
+    % response in the population without attention
+    this_resp0 = npop(:,this_stim);
     all_p0(all_stim(1,ii),all_stim(2,ii),all_stim(3,ii)) = vecnorm(this_resp0(:,1)-this_resp0(:,2));
 end
-toc
 
-c = nan(3,10);
-for dd=2:10
-    [mds0,c(1,dd)] = mdscale(mean(all_p0,3),dd,'Options',statset('MaxIter',1e4));
-    [mds1,c(2,dd)] = mdscale(mean(all_p1,3),dd,'Options',statset('MaxIter',1e4));
-    [mds2,c(3,dd)] = mdscale(mean(all_p2,3),dd,'Options',statset('MaxIter',1e4));
-end
-
-mds0 = mdscale(mean(all_p0,3),4,'Options',statset('MaxIter',1e4));
-mds1 = mdscale(mean(all_p1,3),4,'Options',statset('MaxIter',1e4));
-mds2 = mdscale(mean(all_p2,3),4,'Options',statset('MaxIter',1e4));
+% multidimensional scaling
+mds0 = mdscale(mean(all_p0,3,'omitnan'),4,'Options',statset('MaxIter',1e4));
+mds1 = mdscale(mean(all_p1,3,'omitnan'),4,'Options',statset('MaxIter',1e4));
+mds2 = mdscale(mean(all_p2,3,'omitnan'),4,'Options',statset('MaxIter',1e4));
 
 [~,mds1P] = procrustes(mds0,mds1,'Scaling',false);
 [~,mds2P] = procrustes(mds0,mds2,'Scaling',false);
 
+% collate solutions
+d = nan([size(mds0) 3]);
+d(:,:,1) = mds0;
+d(:,:,2) = mds1P;
+d(:,:,3) = mds2P;
+
 % 2-D plots
 figure, hold on
-plot(mds0(:,1),mds0(:,2),'k--','LineWidth',1)
-plot(mds1P(:,1),mds1P(:,2),'r-','LineWidth',1)
-plot(mds2P(:,1),mds2P(:,2),'b-','LineWidth',1)
-plot(mds0(15:15:end,1),mds0(15:15:end,2),'k.','MarkerSize',10)
-plot(mds0(45,1),mds0(45,2),'k.','MarkerSize',20)
-plot(mds1P(15:15:end,1),mds1P(15:15:end,2),'r.','MarkerSize',10)
-plot(mds1P(45,1),mds1P(45,2),'r.','MarkerSize',20)
-plot(mds2P(15:15:end,1),mds2P(15:15:end,2),'b.','MarkerSize',10)
-plot(mds2P(45,1),mds2P(45,2),'b.','MarkerSize',20)
+p1 = plot(squeeze([d(:,1,:);d(1,1,:)]),squeeze([d(:,2,:);d(1,2,:)]),'LineWidth',1);
+p2 = plot(squeeze(d(15:15:end,1,:)),squeeze(d(15:15:end,2,:)),'.','MarkerSize',10);
+p3 = plot(squeeze(d(45,1,:)),squeeze(d(45,2,:)),'.','MarkerSize',20);
+p1(1).Color = 'k';
+p1(1).LineStyle = '--';
+p1(2).Color = 'r';
+p1(3).Color = 'b';
+axis([-.3 .3 -.3 .3])
 axis equal
 legend({'No attention','Gain only','Gain + Normalization'})
 
-figure, hold on
-plot(mds0(:,3),mds0(:,4))
-plot(mds1P(:,3),mds1P(:,4))
-plot(mds2P(:,3),mds2P(:,4))
-axis equal
+% sliding window
+clear cond_length
+wind = 20; % ±4 stim around each orientation
+for ii=length(triad_stim):-1:1
+    this_set = mod(ii-wind:wind+ii,length(triad_stim));
+    this_set(this_set==0) = length(triad_stim);
+    cond_length(:,ii) = squeeze(sum(vecnorm(diff(d(this_set,:,:),[],1),2,2),1));
+end
 
-% lengths
 figure, hold on
-plot(ang(1:end-1),diag(mean(all_p0,3),1),'k--','LineWidth',1)
-plot(ang(1:end-1),diag(mean(all_p1,3),1),'r-','LineWidth',1)
-plot(ang(1:end-1),diag(mean(all_p2,3),1),'b-','LineWidth',1)
-%axis([0 180 .02 .04])
+p = plot(triad_stim,cond_length,'LineWidth',1);
+p(1).Color = 'k';
+p(1).LineStyle = '--';
+p(2).Color = 'r';
+p(3).Color = 'b';
+axis([-90 90 .3 .6])
 legend({'No attention','Gain only','Gain + Normalization'})
-
-%%
-% get distance matrices
-npop_distN = squareform(pdist(npop','cosine'));
-npop_distA = squareform(pdist(npop_attn','cosine'));
-
-mds_2N = mdscale(npop_distN,2);
-mds_2A = mdscale(npop_distA,2);
-
-[~,mds_2A] = procrustes(mds_2N,mds_2A); % align coordinates
-
-figure, hold on
-plot(mds_2N(:,1),mds_2N(:,2),'r-')
-plot(mds_2N(ang==45,1),mds_2N(ang==45,2),'r.','MarkerSize',20)
-plot(mds_2A(:,1),mds_2A(:,2),'b--')
-plot(mds_2A(ang==45,1),mds_2A(ang==45,2),'b.','MarkerSize',20)
-axis equal
